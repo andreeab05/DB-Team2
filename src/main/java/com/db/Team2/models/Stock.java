13974b69e240d922640ffc6881acd0ea355e7f89
andreeab05 <andreea.borbei@stud.acs.upb.ro>
2024-07-30 05:25:06 -0700

    Created getters and setters

--- src/main/java/com/db/Team2/models/Stock.java
+++ src/main/java/com/db/Team2/models/Stock.java
@@ -25,4 +25,46 @@ public class Stock {
 
     @Column
     private int growth_rate;
+
+    public int getStock_id() {
+        return stock_id;
+    }
+
+    public void setStock_id(int stock_id) {
+        this.stock_id = stock_id;
+    }
+
+    public int getStock_name() {
+        return stock_name;
+    }
+
+    public void setStock_name(int stock_name) {
+        this.stock_name = stock_name;
+    }
+
+    public String getCountry_market() {
+        return country_market;
+    }
+
+    public void setCountry_market(String country_market) {
+        this.country_market = country_market;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getGrowth_rate() {
+        return growth_rate;
+    }
+
+    public void setGrowth_rate(int growth_rate) {
+        this.growth_rate = growth_rate;
+    }
+
+
 }
